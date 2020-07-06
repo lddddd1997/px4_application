eb280de6551a76632caf316e4acf47c21b603ea9
lddddd1997 <lddddd1997@gmail.com>
2020-07-06 16:07:26 +0800

    Modify the script

--- script/simulation/collaborative_sim.sh
+++ script/simulation/collaborative_sim.sh
@@ -1,6 +1,7 @@
 ##sitl_gazebo
 gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 2; roslaunch px4 mavros_posix_sitl.launch; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch px4_application base_uav_control.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch px4_application app_uav_collaboration.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch px4_application base_gcs_control.launch; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch px4_application base_gcs_display.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_setting.launch; exec bash"' \
