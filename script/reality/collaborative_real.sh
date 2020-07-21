8aaf308ea76208b7a20aada6a2203eb5f2119484
lddddd1997 <lddddd1997@gmail.com>
2020-07-21 16:05:55 +0800

    Modify the script of reality

--- script/reality/collaborative_real.sh
+++ script/reality/collaborative_real.sh
@@ -2,5 +2,6 @@
 gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch mavros px4.launch fcu_url:="/dev/ttyTHS1:921600"; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch px4_application app_uav_collaboration.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_control.launch; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch px4_application base_uav_control.launch; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_display.launch; exec bash"' \
 #--tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_setting.launch; exec bash"' \
