c3dc719ed097ce45f9d71a174bb6e20de0d8a742
lddddd1997 <lddddd1997@gmail.com>
2020-07-21 19:56:45 +0800

    Add the interface of uav_id

--- script/reality/collaborative_real.sh
+++ script/reality/collaborative_real.sh
@@ -1,6 +1,11 @@
-##sitl_gazebo
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 4; roslaunch mavros px4.launch fcu_url:="/dev/ttyTHS1:921600"; exec bash"' \
---tab -e 'bash -c "sleep 4; roslaunch px4_application app_uav_collaboration.launch; exec bash"' \
---tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_control.launch; exec bash"' \
-#--tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_setting.launch; exec bash"' \
+##Task start script
+# Simulation "udp://:14540@127.0.0.1:14557"
+# Usb        "/dev/ttyACM0:57600"
+# Nano       "/dev/ttyTHS1:921600"
+export uav_id=0
+export fcu_url="/dev/ttyTHS1:921600"
+gnome-terminal --window -e 'bash -c "roslaunch px4_application base_uav_mavros_connection.launch uav_id:=$uav_id fcu_url:=$fcu_url; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application base_uav_control.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application app_uav_collaboration.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_display.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_setting.launch uav_id:=$uav_id; exec bash"' \
--- script/simulation/collaborative_sim.sh
+++ script/simulation/collaborative_sim.sh
@@ -1,7 +1,7 @@
-##sitl_gazebo
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch px4 mavros_posix_sitl.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch px4_application base_uav_control.launch; exec bash"' \
---tab -e 'bash -c "sleep 3; roslaunch px4_application app_uav_collaboration.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; roslaunch px4_application base_gcs_display.launch; exec bash"' \
---tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_setting.launch; exec bash"' \
+##Task start script
+export uav_id=1
+gnome-terminal --window -e 'bash -c "roslaunch px4_application base_uav_mavros_sim.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch px4_application base_uav_control.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch px4_application app_uav_collaboration.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_display.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch px4_application base_gcs_setting.launch uav_id:=$uav_id; exec bash"' \
