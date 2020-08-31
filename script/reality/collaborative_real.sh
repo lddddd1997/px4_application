7b03f1dd69b624478d19b863df073839b05dcf45
lddddd1997 <lddddd1997@gmail.com>
2020-08-31 21:13:47 +0800

    Adjust experimental parameters

--- script/reality/collaborative_real.sh
+++ script/reality/collaborative_real.sh
@@ -3,9 +3,10 @@
 # Usb        "/dev/ttyACM0:57600"
 # Nano       "/dev/ttyTHS1:921600"
 export uav_id=0
-export fcu_url="/dev/ttyACM0:57600"
+export fcu_url="/dev/ttyTHS1:921600"
 gnome-terminal --window -e 'bash -c "roslaunch px4_application base_uav_mavros_connection.launch uav_id:=$uav_id fcu_url:=$fcu_url; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch px4_application base_uav_control.launch uav_id:=$uav_id; exec bash"' \
 --tab -e 'bash -c "sleep 4; roslaunch px4_application app_uav_collaboration.launch uav_id:=$uav_id; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch MonoCamera findObject.launch uav_id:=$uav_id; exec bash"' \
 --tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_display.launch uav_id:=$uav_id; exec bash"' \
 --tab -e 'bash -c "sleep 5; roslaunch px4_application base_gcs_setting.launch uav_id:=$uav_id; exec bash"' \
