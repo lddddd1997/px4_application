a55c83632ac25c5c351f3fc22cdfdaf6215b9b11
lddddd1997 <lddddd1997@gmail.com>
2020-09-28 15:44:26 +0800

    Add TX2 fcu_url

--- script/reality/collaborative_real.sh
+++ script/reality/collaborative_real.sh
@@ -2,6 +2,7 @@
 # Simulation "udp://:14540@127.0.0.1:14557"
 # Usb        "/dev/ttyACM0:57600"
 # Nano       "/dev/ttyTHS1:921600"
+# TX2        "/dev/ttyTHS2:921600"
 export uav_id=0
 export fcu_url="/dev/ttyTHS1:921600"
 gnome-terminal --window -e 'bash -c "roslaunch px4_application base_uav_mavros_connection.launch uav_id:=$uav_id fcu_url:=$fcu_url; exec bash"' \
